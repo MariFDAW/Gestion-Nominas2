7d244caed8a9008a22fb217c30fc2ebd564d906f
MariFDAW <mfargav343@g.educaand.es>
2025-09-24 20:28:31 +0200

    Metodos para el apartado 5
    falta 5.3,5.5 y 5.6

--- nominas/src/Laboral/CalculaNominas.java
+++ nominas/src/Laboral/CalculaNominas.java
@@ -1,9 +1,18 @@
 package Laboral;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import GestorFicheros.GestorFicheroEmpleados;
 import MetodosBD.AltaEmpleado;
+import MetodosBD.ConexionBD;
 
 public class CalculaNominas {
 
@@ -43,5 +52,95 @@ public class CalculaNominas {
                 + " Categoria: " + e1.getCategoria() + " Años: " + e1.anyos + " Sueldo: " + n.sueldo(e1));
         System.out.println("Empleado 2: Nombre: " + e2.nombre + " dni: " + e2.dni + " Sexo:" + e2.sexo
                 + " Categoria: " + e2.getCategoria() + " Años: " + e2.anyos + " Sueldo: " + n.sueldo(e2));
+    }
+
+    private static void mostrarEmpleados(){
+        try (Connection conn = ConexionBD.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM empleados");) {
+            
+            while(rs.next()){
+                System.out.println("Nombre: "+rs.getString(1)+", DNI: "+rs.getString(2)+", Sexo: "+rs.getString(3)
+                +" ,Categoria: "+rs.getInt(4)+" ,Años: "+rs.getInt(5));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void mostrarUnSoloEmpleado(String dni){
+        try (Connection conn = ConexionBD.getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM empleados where dni = ?");) {
+            
+            ps.setString(1, dni);   
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                System.out.println("Nombre: "+rs.getString(1)+", DNI: "+rs.getString(2)+", Sexo: "+rs.getString(3)
+                +" ,Categoria: "+rs.getInt(4)+" ,Años: "+rs.getInt(5));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Revisar
+    private static void actualizarEmpleados(Empleado emp){
+        String update = "UPDATE empleados SET Nombre = ?, SET DNI = ?, SET SEXO = ?,SET Categoria = ?,SET Años = ? WHERE DNI = ?";
+        try(Connection conn = ConexionBD.getConnection();
+            Statement st = conn.createStatement();
+            PreparedStatement ps = conn.prepareStatement(update)) {
+            
+            ps.setString(1, emp.nombre);
+            ps.setString(2, emp.dni);
+            ps.setString(3, String.valueOf(emp.sexo));
+            ps.setInt(4, emp.getCategoria());
+            ps.setInt(5, emp.anyos);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    private static void actualizarSueldoUnEmpleado(Empleado emp){
+        String update = "UPDATE nominas SET sueldo = ? WHERE empleado = ?";
+        Nomina n = new Nomina();
+        try(Connection conn = ConexionBD.getConnection();
+            Statement st = conn.createStatement();
+            PreparedStatement ps = conn.prepareStatement(update)) {
+            
+            ps.setDouble(1, n.sueldo(emp));
+            ps.setString(2, emp.dni);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    private static void actualizarSueldosTodosEmpleados(){
+        
+    }
+
+    private static void copiaBDEnFichero(){
+        String ruta = "data\\copiaBD";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ruta));
+             Connection conn = ConexionBD.getConnection();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM empleados");) {
+            
+            
+
+        } catch (SQLException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    
     }
 }
